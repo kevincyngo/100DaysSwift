6b72c6ac0163aa37a86f4dd1bfd89c136da233dd
Kevin Ngo <ngokevin39@gmail.com>
2020-03-22 12:42:26 -0400

    Project 7 initial commit

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Kevin Ngo on 2020-03-22.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
