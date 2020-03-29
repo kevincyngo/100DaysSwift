f6e55bae4788d0789feb037f84514f59c5ea6905
Kevin Ngo <ngokevin39@gmail.com>
2020-03-28 23:40:04 -0400

    Project 10 challenges

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -63,20 +63,28 @@ UINavigationControllerDelegate {
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
-
-        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
-        ac.addTextField()
-
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-
-        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
-            guard let newName = ac?.textFields?[0].text else { return }
-            person.name = newName
-
+        let promptAC = UIAlertController(title: "Delete or Rename", message: "Select desired option", preferredStyle: .alert)
+        
+        promptAC.addAction(UIAlertAction(title: "Delete", style: .default){
+            [weak self] _ in
+            self!.people.remove(at: indexPath.item)
             self?.collectionView.reloadData()
+            
         })
-
-        present(ac, animated: true)
+        
+        promptAC.addAction(UIAlertAction(title: "Rename", style: .default){
+            [weak self] _ in
+            let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+                guard let newName = ac?.textFields?[0].text else { return }
+                person.name = newName
+                self?.collectionView.reloadData()
+            })
+            self!.present(ac, animated: true)
+        })
+        present(promptAC, animated: true)
     }
     
     
@@ -88,8 +96,13 @@ UINavigationControllerDelegate {
     @objc func addNewPerson() {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        }
         picker.delegate = self
         present(picker, animated: true)
+        
+        
     }
 
 }
