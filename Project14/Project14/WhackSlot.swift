c33d50f7186e757a8dd3dbc6cb180100798ef0fd
Kevin Ngo <ngokevin39@gmail.com>
2020-04-05 19:45:54 -0400

    Day 55 complete

---
+++ Project14/Project14/WhackSlot.swift
@@ -0,0 +1,67 @@
+//
+//  WhackSlot.swift
+//  Project14
+//
+//  Created by Kevin Ngo on 2020-04-05.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+//contains the hole, penguin, and more
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    var isVisible = false
+    var isHit = false
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+        
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        //Calls hide on itself after a deadline
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+}
