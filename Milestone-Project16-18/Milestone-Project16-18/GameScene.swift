b057b38b0a9cf14a2c39456a3b27a60fac1c7e91
Kevin Ngo <ngokevin39@gmail.com>
2020-04-13 16:31:38 -0400

    added 3 bullet max

--- Milestone-Project16-18/Milestone-Project16-18/GameScene.swift
+++ Milestone-Project16-18/Milestone-Project16-18/GameScene.swift
@@ -25,6 +25,10 @@ class GameScene: SKScene {
         }
     }
         
+    var bulletsSprites = [SKSpriteNode]()
+    
+    var bulletsRemaining = 3
+    
     var gameTimer: Timer?
     
     let MAX_VISIBLE_WIDTH = 1024
@@ -44,15 +48,41 @@ class GameScene: SKScene {
         
         //Add score tracking
         scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.position = CGPoint(x: 32, y: 32)
         scoreLabel.horizontalAlignmentMode = .left
         addChild(scoreLabel)
         score = 0
         
+        let shots0 = SKSpriteNode(imageNamed: "shots0")
+        shots0.position = CGPoint(x: 950, y: 32)
+        
+        let shots1 = SKSpriteNode(imageNamed: "shots1")
+        shots1.position = CGPoint(x: 950, y: 32)
+        
+        let shots2 = SKSpriteNode(imageNamed: "shots2")
+        shots2.position = CGPoint(x: 950, y: 32)
+        
+        let shots3 = SKSpriteNode(imageNamed: "shots3")
+        shots3.position = CGPoint(x: 950, y: 32)
+        
+        bulletsSprites.append(shots0)
+        bulletsSprites.append(shots1)
+        bulletsSprites.append(shots2)
+        bulletsSprites.append(shots3)
+
+        addChild(bulletsSprites[0])
+        addChild(bulletsSprites[1])
+        addChild(bulletsSprites[2])
+        addChild(bulletsSprites[3])
+        
+        bulletsSprites[0].isHidden = true
+        bulletsSprites[1].isHidden = true
+        bulletsSprites[2].isHidden = true
+        
         timerLabel = SKLabelNode(fontNamed: "Chalkduster")
         timerLabel.position = CGPoint(x:MAX_VISIBLE_WIDTH/2, y: MAX_VISIBLE_HEIGHT - 50)
         addChild(timerLabel)
-        timer = 5
+        timer = 60
         
         gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(createTarget), userInfo: nil, repeats: true)
         
@@ -63,9 +93,9 @@ class GameScene: SKScene {
         let startingPosition = Int.random(in: 0...3)
         let movingTarget = MovingTarget()
         if startingPosition % 2 == 0 {
-            movingTarget.configure(at: CGPoint(x: LEFT_SPAWN_WIDTH, y: 150 + row*200), isLeftSpawn: true)
+            movingTarget.configure(at: CGPoint(x: LEFT_SPAWN_WIDTH, y: 250 + row*100), isLeftSpawn: true)
         } else {
-            movingTarget.configure(at: CGPoint(x: RIGHT_SPAWN_WIDTH, y: 150 + row*200), isLeftSpawn: false)
+            movingTarget.configure(at: CGPoint(x: RIGHT_SPAWN_WIDTH, y: 250 + row*100), isLeftSpawn: false)
         }
         addChild(movingTarget)
         movingTarget.move()
@@ -101,20 +131,13 @@ class GameScene: SKScene {
         addChild(finalScoreLabel)
     }
     
-    //
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        /*
-         for node in children {
-             if node.position.x < -300 {
-                 node.removeFromParent()
-             }
-         }
 
-         if !isGameOver {
-             score += 1
-         }
-         */
+    func reload(_ bulletAmt: Int) {
+        for bullet in bulletsSprites {
+            bullet.isHidden = true
+        }
+        bulletsSprites[bulletAmt].isHidden = false
+        
     }
     
     //this method finds any touches, find where it was tapped, then get node array of all nodes at that point
@@ -123,6 +146,16 @@ class GameScene: SKScene {
         let location = touch.location(in: self)
         let tappedNodes = nodes(at: location)
         
+        if location.y <= 200 || location.y >= 600 {
+            bulletsRemaining = 3
+        } else {
+            if bulletsRemaining == 0 {
+                return
+            }
+            bulletsRemaining = max(0, bulletsRemaining-1)
+        }
+        reload(bulletsRemaining)
+
         // guard let whackSlot = node.parent?.parent as? WhackSlot else { continue }
         for node in tappedNodes {
             guard let target = node as? MovingTarget else { continue }
