3966ba625dcb4cac095836f99419af05c7020507
Kevin Ngo <ngokevin39@gmail.com>
2020-05-06 12:40:58 -0400

    Milestone project 28-30 initial commit

---
+++ Milestone-Project28-30/Milestone-Project28-30/Controller/GameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Milestone-Project28-30
+//
+//  Created by Kevin Ngo on 2020-05-06.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Memorize This!"
+    }
+
+
+}
+
---
+++ Milestone-Project28-30/Milestone-Project28-30/Model/CardCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCell.swift
+//  Milestone-Project28-30
+//
+//  Created by Kevin Ngo on 2020-05-06.
+//  Copyright © 2020 Kevin Ngo. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    
+}
